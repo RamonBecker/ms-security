d4295d9b6f6323a3a402223ab4b4d479710de3b2
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2025-02-24 13:59:14 -0300

    Configuring users using InMemoryUserDetailsManager

--- src/main/java/com/eazybytes/springsecurity/infraestructure/configuration/SecurityConfig.java
+++ src/main/java/com/eazybytes/springsecurity/infraestructure/configuration/SecurityConfig.java
@@ -4,6 +4,10 @@ package com.eazybytes.springsecurity.infraestructure.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static com.eazybytes.springsecurity.infraestructure.commom.AccountUrlCommom.ACCOUNT_URL;
@@ -27,10 +31,22 @@ public class SecurityConfig {
                 .requestMatchers(ACCOUNT_URL, TRASACTION_USER_DETAILS_URL, LOAN_USER_URL, CARD_URL).authenticated()
                 .requestMatchers(NOTICE_URL, CONTACT_URL, "/error").permitAll());
 
+//        http.formLogin(AbstractHttpConfigurer::disable);
+//        http.httpBasic(AbstractHttpConfigurer::disable);
+
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        var user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        var admin = User.withUsername("admin").password("{noop}12345").authorities("admin").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
